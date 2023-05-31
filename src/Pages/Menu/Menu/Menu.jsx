bbce7de68b55374bb14a7ebcd7b29a4969b95441
z3san <ahtisamhaque@gmail.com>
2023-05-31 14:45:46 +0600

    dynamic cover implemented

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Helmet  } from 'react-helmet-async';
-
+import Cover from '../../Shared/Cover/Cover';
+import menuImage from "../../../assets/menu/banner3.jpg"
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover img={menuImage} title={"our menu"}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImage} title={"our menu"}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImage} title={"our menu"}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
