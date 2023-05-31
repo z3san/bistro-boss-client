d7f3b7b173ccd8581a596729f131db4fd15e473c
z3san <ahtisamhaque@gmail.com>
2023-05-31 16:06:50 +0600

    menu items created and fetched dynamically

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -2,18 +2,43 @@ import React from 'react';
 import { Helmet  } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImage from "../../../assets/menu/banner3.jpg"
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import dessertImage from "../../../assets/menu/dessert-bg.jpeg"
+import pizzaImage from "../../../assets/menu/pizza-bg.jpg"
+import soupImage from "../../../assets/menu/soup-bg.jpg"
+import saladImage from "../../../assets/menu/salad-bg.jpg"
+import useMenu from '../../../Hooks/UseMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 const Menu = () => {
+
+    const [menu] = useMenu()
+    const offered = menu.filter(item=>item.category=== 'offered')
+    const desserts = menu.filter(item=>item.category=== 'dessert')
+    const soup = menu.filter(item=>item.category=== 'soup')
+    const salad = menu.filter(item=>item.category=== 'salad')
+    const pizza = menu.filter(item=>item.category=== 'pizza')
+
     return (
-        
+
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImage} title={"our menu"}></Cover>
+            {/* main cover */}
+            <SectionTitle subHeading="Dont Miss Todays Offer!" heading="Todays Offer" />
+            {/* Offered items */}
+            <MenuCategory items={offered} />
+            {/* Dessert Items */}
+            <SectionTitle subHeading="" heading=""/>
+            <MenuCategory items={desserts} img={dessertImage} title={"dessert"}/>
+            <MenuCategory items={pizza} img={pizzaImage} title="pizza"  />
+            <MenuCategory items={salad} img={saladImage} title="salad"  />
+            <MenuCategory items={soup} img={soupImage} title="pizza"  />
+          
            
         </div>
     );
-};
+    }
 
 export default Menu;
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import MenuItems from '../../Shared/MenuItems/MenuItems';
+import Cover from '../../Shared/Cover/Cover';
 
-const MenuCategory = () => {
+
+
+const MenuCategory = ({items, title, img}) => {
     return (
-        <div>
-            
-        </div>
+
+      <div className='pt-8'>
+        {title && <Cover img={img} title={title}/>}
+          <div className='grid md: grid-cols-2 gap-10 my-16'>
+        {
+            items.map(item=><MenuItems item={item} key={item._id} ></MenuItems>)
+        }
+    </div>
+      </div>
     );
 };
 
